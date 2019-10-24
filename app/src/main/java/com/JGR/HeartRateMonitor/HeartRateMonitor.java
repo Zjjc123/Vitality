c19aebe0f04fa224a16dbc148ecf77f5d90998c9
Rohan Krishnan <rohankrishnan@live.com>
2019-10-23 21:32:03 -0700

    Says scanning when finger on camera

--- app/src/main/java/com/JGR/HeartRateMonitor/HeartRateMonitor.java
+++ app/src/main/java/com/JGR/HeartRateMonitor/HeartRateMonitor.java
@@ -169,6 +169,7 @@ public class HeartRateMonitor extends AppCompatActivity {
             }
             else {
                 fingerOn = true;
+                text.setText("Scanning...");
 //                if (imgAvg == 0 || imgAvg == 255) {
 //                    processing.set(false);
 //                    return;
