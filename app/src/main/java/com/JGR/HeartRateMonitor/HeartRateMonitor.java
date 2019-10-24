59df0fb5b436dbaa8192b771646ced1eeaba0d8a
Jason Zhang <zjjc123@hotmail.com>
2019-10-23 21:28:06 -0700

    Simple Finger Detection

--- app/src/main/java/com/JGR/HeartRateMonitor/HeartRateMonitor.java
+++ app/src/main/java/com/JGR/HeartRateMonitor/HeartRateMonitor.java
@@ -66,6 +66,7 @@ public class HeartRateMonitor extends AppCompatActivity {
     private static final int[] beatsArray = new int[beatsArraySize];
     private static double beats = 0;
     private static long startTime = 0;
+    private static boolean fingerOn = false;
 
     /**
      * {@inheritDoc}
@@ -159,73 +160,82 @@ public class HeartRateMonitor extends AppCompatActivity {
 
             int imgAvg = ImageProcessing.decodeYUV420SPtoRedAvg(data.clone(), height, width);
             // Log.i(TAG, "imgAvg="+imgAvg);
-            if (imgAvg == 0 || imgAvg == 255) {
+            if (imgAvg < 200)
+            {
+                fingerOn = false;
+                text.setText("Place finger on camera!");
                 processing.set(false);
                 return;
             }
-
-            int averageArrayAvg = 0;
-            int averageArrayCnt = 0;
-            for (int i = 0; i < averageArray.length; i++) {
-                if (averageArray[i] > 0) {
-                    averageArrayAvg += averageArray[i];
-                    averageArrayCnt++;
-                }
-            }
-
-            int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
-            TYPE newType = currentType;
-            if (imgAvg < rollingAverage) {
-                newType = TYPE.RED;
-                if (newType != currentType) {
-                    beats++;
-                    // Log.d(TAG, "BEAT!! beats="+beats);
-                }
-            } else if (imgAvg > rollingAverage) {
-                newType = TYPE.GREEN;
-            }
-
-            if (averageIndex == averageArraySize) averageIndex = 0;
-            averageArray[averageIndex] = imgAvg;
-            averageIndex++;
-
-            // Transitioned from one state to another to the same
-            if (newType != currentType) {
-                currentType = newType;
-                image.postInvalidate();
-            }
-
-            long endTime = System.currentTimeMillis();
-            double totalTimeInSecs = (endTime - startTime) / 1000d;
-            if (totalTimeInSecs >= 10) {
-                double bps = (beats / totalTimeInSecs);
-                int dpm = (int) (bps * 60d);
-                if (dpm < 30 || dpm > 180) {
-                    startTime = System.currentTimeMillis();
-                    beats = 0;
-                    processing.set(false);
-                    return;
-                }
-
-                // Log.d(TAG,
-                // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
-
-                if (beatsIndex == beatsArraySize) beatsIndex = 0;
-                beatsArray[beatsIndex] = dpm;
-                beatsIndex++;
-
-                int beatsArrayAvg = 0;
-                int beatsArrayCnt = 0;
-                for (int i = 0; i < beatsArray.length; i++) {
-                    if (beatsArray[i] > 0) {
-                        beatsArrayAvg += beatsArray[i];
-                        beatsArrayCnt++;
+            else {
+                fingerOn = true;
+//                if (imgAvg == 0 || imgAvg == 255) {
+//                    processing.set(false);
+//                    return;
+//                }
+                int averageArrayAvg = 0;
+                int averageArrayCnt = 0;
+                for (int i = 0; i < averageArray.length; i++) {
+                    if (averageArray[i] > 0) {
+                        averageArrayAvg += averageArray[i];
+                        averageArrayCnt++;
                     }
                 }
-                int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
-                text.setText(String.valueOf(beatsAvg));
-                startTime = System.currentTimeMillis();
-                beats = 0;
+
+                int rollingAverage = (averageArrayCnt > 0) ? (averageArrayAvg / averageArrayCnt) : 0;
+                TYPE newType = currentType;
+                if (imgAvg < rollingAverage) {
+                    newType = TYPE.RED;
+                    if (newType != currentType) {
+                        beats++;
+                        // Log.d(TAG, "BEAT!! beats="+beats);
+                    }
+                } else if (imgAvg > rollingAverage) {
+                    newType = TYPE.GREEN;
+                }
+
+                if (averageIndex == averageArraySize) averageIndex = 0;
+                averageArray[averageIndex] = imgAvg;
+                averageIndex++;
+
+                // Transitioned from one state to another to the same
+                if (newType != currentType) {
+                    currentType = newType;
+                    image.postInvalidate();
+                }
+
+                long endTime = System.currentTimeMillis();
+                double totalTimeInSecs = (endTime - startTime) / 1000d;
+                if (totalTimeInSecs >= 10) {
+                    double bps = (beats / totalTimeInSecs);
+                    int dpm = (int) (bps * 60d);
+                    if (dpm < 30 || dpm > 180) {
+                        startTime = System.currentTimeMillis();
+                        beats = 0;
+                        processing.set(false);
+                        return;
+                    }
+
+                    // Log.d(TAG,
+                    // "totalTimeInSecs="+totalTimeInSecs+" beats="+beats);
+
+                    if (beatsIndex == beatsArraySize) beatsIndex = 0;
+                    beatsArray[beatsIndex] = dpm;
+                    beatsIndex++;
+
+                    int beatsArrayAvg = 0;
+                    int beatsArrayCnt = 0;
+                    for (int i = 0; i < beatsArray.length; i++) {
+                        if (beatsArray[i] > 0) {
+                            beatsArrayAvg += beatsArray[i];
+                            beatsArrayCnt++;
+                        }
+                    }
+                    int beatsAvg = (beatsArrayAvg / beatsArrayCnt);
+                    text.setText(String.valueOf(beatsAvg));
+                    startTime = System.currentTimeMillis();
+                    beats = 0;
+                }
             }
             processing.set(false);
         }
