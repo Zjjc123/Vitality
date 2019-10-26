bccd109f0dfa697a7f44bb8a85625b8070f7dfd6
Rodahan <rohankrishnan@live.com>
2019-10-26 16:27:42 -0700

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/JGR/HeartRateMonitor/ui/ProfileFragment.java
+++ app/src/main/java/com/JGR/HeartRateMonitor/ui/ProfileFragment.java
@@ -24,24 +24,98 @@ public class ProfileFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        weight = root.findViewById(R.id.in_weight);
-        height = root.findViewById(R.id.in_height);
-        age = root.findViewById(R.id.in_age);
+        weight = root.findViewById(R.id.et_weight);
+        height = root.findViewById(R.id.et_height);
+        age = root.findViewById(R.id.et_age);
+        name = root.findViewById(R.id.et_name);
 
-        disp_weight = root.findViewById(R.id.disp_weight);
-        disp_height = root.findViewById(R.id.disp_height);
-        disp_age = root.findViewById(R.id.disp_age);
+//        disp_weight = root.findViewById(R.id.disp_weight);
+//        disp_height = root.findViewById(R.id.disp_height);
+//        disp_age = root.findViewById(R.id.disp_age);
 
         showValues();
 
-        Button bt_calculate = root.findViewById(R.id.bt_calculate);
+//        Button bt_calculate = root.findViewById(R.id.bt_calculate);
+//
+//        tv_result = root.findViewById(R.id.tv_result);
+//
+//        bt_calculate.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                storeValues();
+//            }
+//        });
 
-        tv_result = root.findViewById(R.id.tv_result);
+        weight.addTextChangedListener(new TextWatcher() {
 
-        bt_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void afterTextChanged(Editable s) {
                 storeValues();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+
+            }
+        });
+
+        height.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                storeValues();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+
+            }
+        });
+
+        age.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                storeValues();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+
+            }
+        });
+
+        name.addTextChangedListener(new TextWatcher() {
+
+            // the user's changes are saved here
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+                storeValues();
+            }
+
+            public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+                // this space intentionally left blank
+            }
+
+            public void afterTextChanged(Editable c) {
+                // this one too
             }
         });
 
@@ -53,6 +127,7 @@ public class ProfileFragment extends Fragment {
     private EditText weight;
     private EditText height;
     private EditText age;
+    private EditText name;
 
     SharedPreferences sharedPref;
 
@@ -82,6 +157,11 @@ public class ProfileFragment extends Fragment {
             editor.putInt("age", user_age);
         }
 
+        if (!(name.getText().toString().matches(""))) {
+            String user_name = name.getText().toString();
+            editor.putString("name", user_name);
+        }
+
         editor.apply();
 
         // int restHR = sharedPref.getInt("rest_hr", 80);
@@ -104,15 +184,19 @@ public class ProfileFragment extends Fragment {
 
     private void showValues() {
         SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-        int weight = sharedPref.getInt("weight", 0);
-        int height = sharedPref.getInt("height", 0);
-        int age = sharedPref.getInt("age", 0);
+        int weight_val = sharedPref.getInt("weight", 0);
+        int height_val = sharedPref.getInt("height", 0);
+        int age_val = sharedPref.getInt("age", 0);
+        String name_val = sharedPref.getString("name", "default");
 
+        weight.setText(checkDefault(weight_val));
+        height.setText(checkDefault(height_val));
+        age.setText(checkDefault(age_val));
+        name.setText(name_val);
 
-
-        disp_weight.setText(checkDefault(weight));
-        disp_height.setText(checkDefault(height));
-        disp_age.setText(checkDefault(age));
+//        disp_weight.setText(checkDefault(weight));
+//        disp_height.setText(checkDefault(height));
+//        disp_age.setText(checkDefault(age));
 
     }
 
