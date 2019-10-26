17f828379b984044fddf298d07f187befa275172
Rodahan <rohankrishnan@live.com>
2019-10-26 14:50:07 -0700

    store values in "myprofile"

--- app/src/main/java/com/JGR/HeartRateMonitor/ui/ProfileFragment.java
+++ app/src/main/java/com/JGR/HeartRateMonitor/ui/ProfileFragment.java
@@ -1,7 +1,10 @@
 package com.JGR.HeartRateMonitor.ui;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +24,15 @@ public class ProfileFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        weight = root.findViewById(R.id.in_weight);
+        height = root.findViewById(R.id.in_height);
+        age = root.findViewById(R.id.in_age);
 
-        input1 = root.findViewById(R.id.in_weight);
-        input2 = root.findViewById(R.id.in_height);
-        input3 = root.findViewById(R.id.in_age);
+        disp_weight = root.findViewById(R.id.disp_weight);
+        disp_height = root.findViewById(R.id.disp_height);
+        disp_age = root.findViewById(R.id.disp_age);
+
+        showValues();
 
         Button bt_calculate = root.findViewById(R.id.bt_calculate);
 
@@ -33,36 +41,87 @@ public class ProfileFragment extends Fragment {
         bt_calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                makeCalculations();
+                storeValues();
             }
         });
+
 
         return root;
     }
 
 
-    private EditText input1;
-    private EditText input2;
-    private EditText input3;
+    private EditText weight;
+    private EditText height;
+    private EditText age;
 
     SharedPreferences sharedPref;
 
     private TextView tv_result;
+    private TextView disp_weight;
+    private TextView disp_height;
+    private TextView disp_age;
 
-    private void makeCalculations() {
-        // I'm assuming you're getting numbers.
-        double weight = Double.valueOf(input1.getText().toString());
-        double height = Double.valueOf(input2.getText().toString());
-        double age = Double.valueOf(input3.getText().toString());
+
+    private void storeValues() {
+
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        if (!(weight.getText().toString().matches(""))) {
+            int user_weight = Integer.valueOf(weight.getText().toString());
+            editor.putInt("weight", user_weight);
+        }
+
+        if (!(height.getText().toString().matches(""))) {
+            int user_height = Integer.valueOf(height.getText().toString());
+            editor.putInt("height", user_height);
+        }
+
+        if (!(age.getText().toString().matches(""))) {
+            int user_age = Integer.valueOf(age.getText().toString());
+            editor.putInt("age", user_age);
+        }
+
+        editor.apply();
 
         // int restHR = sharedPref.getInt("rest_hr", 80);
 
-        double bmi = (703*weight)/(height*height);
-        double hrMax = 207-(0.7*age);
+//        double bmi = (703*user_weight)/(user_height*user_height);
+//        double hrMax = 207-(0.7*user_age);
         // Do your calculation here.
         // I'm assuming you have inserted the result on a variable called 'result'. Like: double result
 
-        tv_result.setText("BMI: " + bmi);
+        tv_result.setText("Values Stored");
+
+        showValues();
+
+
+//        weight.setText(user_weight);
+//        height.setText(user_height);
+//        age.setText(user_age);
+
+    }
+
+    private void showValues() {
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        int weight = sharedPref.getInt("weight", 0);
+        int height = sharedPref.getInt("height", 0);
+        int age = sharedPref.getInt("age", 0);
+
+
+
+        disp_weight.setText(checkDefault(weight));
+        disp_height.setText(checkDefault(height));
+        disp_age.setText(checkDefault(age));
+
+    }
+
+    private String checkDefault(int key) {
+        if (key == 0) {
+            return "Please Enter Your Value Below";
+        } else {
+            return Integer.toString(key);
+        }
     }
 
     // The rest of your Activity and methods.
