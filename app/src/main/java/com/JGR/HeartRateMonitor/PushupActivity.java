e26c362c165ffb725ec825950367738a8b2102f7
Rodahan <rohankrishnan@live.com>
2019-10-26 17:56:04 -0700

    semi functioning pushup

--- app/src/main/java/com/JGR/HeartRateMonitor/PushupActivity.java
+++ app/src/main/java/com/JGR/HeartRateMonitor/PushupActivity.java
@@ -3,11 +3,19 @@ package com.JGR.HeartRateMonitor;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,9 +25,32 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.appcompat.widget.Toolbar;
 
-public class PushupActivity extends AppCompatActivity {
+public class PushupActivity extends AppCompatActivity implements SensorEventListener {
 
     Toolbar toolbar;
+
+    private TextView textView;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private View view;
+
+    private TextView totalText;
+    private TextView counter;
+
+    private float downValue = 2.0f;
+
+    private int count = 0;
+
+    private int total;
+    SharedPreferences totalCount;
+
+    private boolean downState = false;
+
+    private int downColor = Color.GREEN;
+    private int upColor = Color.DKGRAY;
+
+    public static final String PREFS_NAME = "MyPrefsFile";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +61,27 @@ public class PushupActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        totalCount = getSharedPreferences(PREFS_NAME,0);
+
+
+        textView = findViewById(R.id.textView);
+        counter = findViewById(R.id.counter);
+        totalText = findViewById(R.id.total);
+
+
+        view = this.getWindow().getDecorView();
+        view.setBackgroundColor(upColor);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        total = totalCount.getInt("total", total);
+
+
+        counter.setText(Integer.toString(count));
+        totalText.setText("Total " + Integer.toString(total));
+
     }
 
     @Override
@@ -46,13 +98,59 @@ public class PushupActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onResume() {
+    protected void onResume(){
         super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
-    public void onPause() {
+    protected void onPause(){
         super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        if(event.sensor.getType() == Sensor.TYPE_PROXIMITY){
+            if(event.values[0] < downValue){
+                pushUpDown();
+            } else {
+                pushUpUp();
+            }
+
+            textView.setText(Float.toString(event.values[0]));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    }
+
+
+    public void pushUpDown(){
+        if (!downState){
+            downState = true;
+            view.setBackgroundColor(downColor);
+
+            count++;
+            counter.setText(Integer.toString(count));
+
+            total++;
+            SharedPreferences.Editor editor = totalCount.edit();
+            editor.putInt("total", total);
+            editor.commit();
+
+            totalText.setText("Total " + Integer.toString(total));
+
+        }
+    }
+
+    public void pushUpUp(){
+        if (downState){
+            downState = false;
+            view.setBackgroundColor(upColor);
+        }
     }
 
 }
--- app/src/main/java/com/JGR/HeartRateMonitor/ui/ProfileFragment.java
+++ app/src/main/java/com/JGR/HeartRateMonitor/ui/ProfileFragment.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -36,7 +37,7 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final View root = inflater.inflate(R.layout.fragment_profile, container, false);
 
         weight = root.findViewById(R.id.et_weight);
         height = root.findViewById(R.id.et_height);
@@ -54,6 +55,7 @@ public class ProfileFragment extends Fragment {
         bt_update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(root.getContext(), "Profile Updated", Toast.LENGTH_SHORT).show();
                 storeValues();
             }
         });
